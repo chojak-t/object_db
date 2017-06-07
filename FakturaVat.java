49b84dfac448a49f038966bd89f6db14042d4e97
satanicus1991 <satanicus1991@gmail.com>
2017-06-07 17:42:28 +0200

    Dodano klase fakturaVat

---
+++ FakturaVat.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package warsztat;
+
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author satan
+ */
+@Entity
+public class FakturaVat extends Faktura{
+    
+}
